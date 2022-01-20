d07c656c84e989ceaddaec13410a515fdb9575b6
Luca Coduri <coduri.vitor@gmail.com>
2022-01-21 00:57:23 +0100

    Triggers

---
+++ scripts/triggers.sql
@@ -0,0 +1,28 @@
+-- Un étudiant ne peut pas avoir un test d'un cours qu'il ne suit pas
+
+CREATE OR REPLACE FUNCTION check_if_user_has_cours()
+    RETURNS TRIGGER LANGUAGE plpgsql as
+$BODY$
+begin
+    if EXISTS(select * FROM etudiant_leçon el INNER JOIN leçon l on l.numéro = el.noleçon and l.idcours = el.idleçon
+WHERE el.idetudiant = NEW.idetudiant AND EXISTS(select * FROM test t WHERE t.id = NEW.idtest AND l.idcours = t.idcours)) then
+        RETURN NEW;
+    else
+        IF(TG_OP = 'UPDATE') THEN
+            RETURN OLD;
+        ELSE
+            RETURN NULL;
+        END IF;
+    end if;
+end;
+$BODY$;
+
+CREATE OR REPLACE TRIGGER before_insert_etudiant_test
+BEFORE INSERT ON etudiant_test
+FOR EACH ROW
+EXECUTE FUNCTION check_if_user_has_cours();
+
+CREATE OR REPLACE TRIGGER before_update_etudiant_test
+BEFORE UPDATE ON etudiant_test
+FOR EACH ROW
+EXECUTE FUNCTION check_if_user_has_cours();
