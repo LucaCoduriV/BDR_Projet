90d531290065d3ab188082cf253a9a65ed11a0e1
hadrylouis <hadry.louis@gmail.com>
2022-01-23 09:19:52 +0100

    [Done] status

--- src/db.php
+++ src/db.php
@@ -615,6 +615,78 @@ class Database
         $sth->execute();
         return $sth->errorInfo();
     }
+
+    function getAllStatus()
+    {
+        $sql = <<<'SQL'
+        SELECT *
+        FROM statut;
+        SQL;
+
+        $sth = $this->connexion->prepare($sql);
+
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    function ajouterStatut($libelle)
+    {
+        $sql = <<<'SQL'
+        INSERT INTO statut (libellé)
+        VALUES (:libelle);
+        SQL;
+
+        $sth = $this->connexion->prepare($sql);
+        $sth->bindParam('libelle', $libelle);
+
+        $sth->execute();
+        return $sth->errorInfo();
+    }
+
+    function supprimerStatut($libelle)
+    {
+        $sql = <<<'SQL'
+        DELETE FROM statut WHERE libellé = :libelle;
+        SQL;
+        
+        $sth = $this->connexion->prepare($sql);
+        $sth->bindParam('libelle', $libelle);
+     
+        $sth->execute();
+
+        return $sth->errorInfo();
+    }
+
+    function getStatus($libelle)
+    {
+        $sql = <<<'SQL'
+        SELECT *
+        FROM statut
+        WHERE libellé = :libelle;
+        SQL;
+
+        $sth = $this->connexion->prepare($sql);
+        $sth->bindParam('libelle', $libelle);
+
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    function modifierStatut($oldlibelle, $libelle)
+    {
+        $sql = <<<'SQL'
+        UPDATE statut
+        SET libellé = :libelle
+        WHERE libellé = :oldlibelle;
+        SQL;
+
+        $sth = $this->connexion->prepare($sql);
+        $sth->bindParam('libelle', $libelle);
+        $sth->bindParam('oldlibelle', $oldlibelle);
+
+        $sth->execute();
+        return $sth->errorInfo();
+    }
 }
 
 $db = new Database();
