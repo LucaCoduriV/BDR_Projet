8866740cfe05373a17ba80b27d2776dd7aa01e68
Luca Coduri <coduri.vitor@gmail.com>
2022-01-28 16:06:27 +0100

    Trigger etudiant professeur

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -26,3 +26,41 @@ CREATE OR REPLACE TRIGGER before_insert_or_update_etudiant_test
 BEFORE INSERT OR UPDATE ON etudiant_test
 FOR EACH ROW
 EXECUTE FUNCTION check_if_user_has_cours();
+
+-- Un élève ne peut pas être un professeur en même temps.
+
+CREATE OR REPLACE FUNCTION check_etudiant_not_professeur()
+    RETURNS TRIGGER LANGUAGE plpgsql AS
+$BODY$
+BEGIN
+    IF NOT EXISTS(SELECT etudiant.idpersonne FROM etudiant INNER JOIN professeur ON NEW.idpersonne = professeur.idpersonne) THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Un étudiant ne peut être un professeur en même temps.';
+    END IF;
+END;
+$BODY$;
+
+CREATE OR REPLACE TRIGGER before_insert_or_update_etudiant
+BEFORE INSERT OR UPDATE ON etudiant
+FOR EACH ROW
+EXECUTE FUNCTION check_etudiant_not_professeur();
+
+-- Un professeur ne peut pas être un élève en même temps.
+
+CREATE OR REPLACE FUNCTION check_professeur_not_etudiant()
+    RETURNS TRIGGER LANGUAGE plpgsql AS
+$BODY$
+BEGIN
+    IF NOT EXISTS(SELECT professeur.idpersonne FROM professeur INNER JOIN etudiant ON etudiant.idpersonne = NEW.idpersonne) THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Un professeur ne peut être un étudiant en même temps.';
+    END IF;
+END;
+$BODY$;
+
+CREATE OR REPLACE TRIGGER before_insert_or_update_professeur
+BEFORE INSERT OR UPDATE ON professeur
+FOR EACH ROW
+EXECUTE FUNCTION check_professeur_not_etudiant();
